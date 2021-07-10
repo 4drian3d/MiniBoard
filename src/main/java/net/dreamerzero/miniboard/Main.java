5f5c9cc554c541a9129f08bacfafe904bc1fbef8
Adrian Gonzales <adrianed2004@gmail.com>
2021-07-10 12:31:35 -0500

    Proyect better organized - Edit pom.xml - Edit Main.java

--- src/main/java/net/dreamerzero/miniboard/Main.java
+++ src/main/java/net/dreamerzero/miniboard/Main.java
@@ -1,15 +1,15 @@
 package net.dreamerzero.miniboard;
 
 import fr.mrmicky.fastboard.FastBoard;
+
 import org.bukkit.ChatColor;
-import org.bukkit.Statistic;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.player;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,59 +17,44 @@ import java.util.UUID;
 
 public class Main extends JavaPlugin implements Listener {
 
-    private final Map<UUID, FastBoard> boards = new HashMap<>();
+	private final Map<UUID, FastBoard> boards = new HashMap<>();
 	
-	@Override
 	public void onEnable(){
 		getServer().getPluginManager().registerEvents(this, this);
 		
+		getLogger().info("<-------------->");
+		getLogger().info("MiniBoard enabling");
+		getLogger().info("<-------------->");
+		
 		getServer().getScheduler().runTaskTimer(this, () -> {
             for (FastBoard board : this.boards.values()) {
-                updateBoard(board);
+                board.updateLines(
+					ChatColor.GRAY + "------------------",
+					ChatColor.DARK_GRAY + "[" + ChatColor.DARK_AQUA + "Perfil" + ChatColor.DARK_GRAY + "]",
+					ChatColor.GRAY + "------------------",
+				);
             }
         }, 0, 20);
-		
-		Bukkit.getConsoleSender().sendMessage("<-------------->\n");
-
-		Bukkit.getConsoleSender().sendMessage("<-------------->");
-    }
+	}
 	
-	@EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        Player player = e.getPlayer();
-
-        FastBoard board = new FastBoard(player);
-
-        board.updateTitle(ChatColor.RED + "Peru" + ChatColor.WHITE + "vian" + ChatColor.RED + "kkit");
+	public void onJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+		
+		FastBoard board = new FastBoard(player);
+		
+		board.updateTitle(ChatColor.RED + "Peru" + ChatColor.WHITE + "vian" + ChatColor.RED + "kkit");
 
         this.boards.put(player.getUniqueId(), board);
-    }
+	}
 	
 	@EventHandler
     public void onQuit(PlayerQuitEvent e) {
         Player player = e.getPlayer();
 
         FastBoard board = this.boards.remove(player.getUniqueId());
-
-        if (board != null) {
+		
+		if (board != null) {
             board.delete();
         }
-    }
-	
-	private void updateBoard(FastBoard board) {
-        board.updateLines(
-                "------------------",
-                "[Perfil]",
-                "4drian3d",
-                "Ping ",
-                "------------------",
-				"Modalidad",
-				"Towny: ",
-				"SkyBlock",
-				"Somos 20 Online",
-				"------------------",
-				"Yo me quedo en PVK",
-				"play.peruviankkit.net"
-        );
-    }
+	}
 }
